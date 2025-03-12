68cde4f90e7bc5e178259cecf8a19917cfaac9ad
nicholasassia <56373579+nicholasassia@users.noreply.github.com>
2025-03-12 04:27:14 -0400

    Add files via upload
    yea

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('solveButton').addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: solvePuzzle
+    });
+});
